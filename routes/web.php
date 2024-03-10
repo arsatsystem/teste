a3f3141b67c5e25470ab40983078c08244957840
Cleudo Silva <arsatmidias@gmail.com>
2024-03-10 00:30:20 +0000

    Controller e Rotas

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,19 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
+
+https://m.pg-nmga.com/
+126/
+index.html
+?btt=1
+&ot=181FF71B-20D7-4E3C-9E2B-F5602D055DA7
+&ops=nashAAA9218563AAAwMMY427Kh4AhtIyxUxYLvdd85s4PHfsX
+&l=pt
+&f=https://m.realsbet.com/casino
+&__refer=m.pgr-nmga.com
+&or=static.pg-nmga.com
+&__hv=1f819bc7
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{path}', [CopyController::class, 'copy'])->where('path', '.*');
+
